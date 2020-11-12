c396566fdd5ae0bb4e09745d0a0450f65665a057
christophe mohammed-cherif <krisfromtama@gmail.com>
2020-11-12 15:30:48 +0100

    show done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_09_090641) do
+ActiveRecord::Schema.define(version: 2020_11_10_153640) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,22 @@ ActiveRecord::Schema.define(version: 2020_11_09_090641) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "images", force: :cascade do |t|
+    t.string "description"
+    t.bigint "animal_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["animal_id"], name: "index_images_on_animal_id"
+  end
+
+  create_table "photos", force: :cascade do |t|
+    t.string "description"
+    t.bigint "animal_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["animal_id"], name: "index_photos_on_animal_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -55,4 +71,6 @@ ActiveRecord::Schema.define(version: 2020_11_09_090641) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "images", "animals"
+  add_foreign_key "photos", "animals"
 end
