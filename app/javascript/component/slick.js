2f668524c47c8459e5dde36f892cfe1caaaa600b
christophe mohammed-cherif <krisfromtama@gmail.com>
2020-11-16 22:18:53 +0100

    slick fonctionne

--- app/javascript/component/slick.js
+++ app/javascript/component/slick.js
@@ -1,11 +1,14 @@
 import "slick-carousel";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 
 const slick = () => {
   $('.autoplay').slick({
       slidesToShow: 1,
       slidesToScroll: 1,
-      autoplay: true,
-      autoplaySpeed: 2000,
+      autoplay: false,
+      autoplaySpeed: 3000,
   });
 }
 
