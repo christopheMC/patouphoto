51b8cadbd07fc498951112e0bf66ecdaf0462bfd
christophe mohammed-cherif <krisfromtama@gmail.com>
2020-11-25 13:02:25 +0100

    rich_body done mais probleme JS pour le contact

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,4 +1,6 @@
 class ImagesController < ApplicationController
+    before_action :set_image, only: [ :destroy ]
+
   def new
     @theme  = Theme.find(params[:theme_id])
     @image = Image.new
@@ -16,12 +18,15 @@ class ImagesController < ApplicationController
   end
 
   def destroy
-    @image = Image.find(params[:id])
     @image.destroy
     redirect_to theme_path(@image.theme)
   end
 
   private
+
+  def set_image
+    @image = Image.find(params[:id])
+  end
 
   def image_params
     params.require(:image).permit(:description, :photo)
---
+++ app/javascript/component/updateContacted.js
@@ -0,0 +1,8 @@
+const updateContacted = () => {
+  const checkboxes = document.querySelectorAll('.form-checkbox');
+  checkboxes.forEach(checkbox => checkbox.addEventListener('click', () => {
+    ;
+  }))
+}
+
+export { updateContacted };
