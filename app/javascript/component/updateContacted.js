4260e1e6e65510d213061be49a90d66f610669de
christophe mohammed-cherif <krisfromtama@gmail.com>
2020-11-30 19:31:23 +0100

    contacted fonctionne mais plus le CSS

--- app/javascript/component/updateContacted.js
+++ app/javascript/component/updateContacted.js
@@ -2,22 +2,23 @@ import { fetchWithToken } from "../utils/fetch_with_token";
 
 const updateContacted = () => {
   const checkboxes = document.querySelectorAll('.form-checkbox');
+  const afficheck = document.querySelectorAll('.affiche');
   checkboxes.forEach(checkbox => checkbox.addEventListener('click', () => {
     event.preventDefault();
+    for (let i = 0; i < afficheck.count; i++) {
+      this.classList.add('para');
+    }
     const contactId = checkbox.dataset.contactId;
 
-    fetchWithToken("/contacts/`${contactId}`/contacted", {
+    fetchWithToken(`/contacts/${contactId}/contacted`, {
       method: "POST",
       headers: {
         "Accept": "application/json",
         "Content-Type": "application/json"
       },
-      body: JSON.stringify({ contact: { contacted: true }})
     })
       .then(response => response.json())
-      .then((data) => {
-
-      });
+      .then(data => console.log(data));
   }));
 }
 
