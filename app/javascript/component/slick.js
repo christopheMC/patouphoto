8bde827fedac0f6b972ef3204a58a60d787e333e
christophe mohammed-cherif <krisfromtama@gmail.com>
2020-12-15 00:23:39 +0100

    correction d un peu de responsive

--- app/javascript/component/slick.js
+++ app/javascript/component/slick.js
@@ -4,8 +4,26 @@ import "slick-carousel/slick/slick-theme.css";
 
 
 const slick = () => {
-  $(document).on('click', () => {
+  const carousel = document.querySelector(".single-item");
+  if (!carousel) return;
+
+  const cards = document.querySelectorAll(".card");
+  cards.forEach((card) => {
+    card.addEventListener("click", (event) => {
+      const id = parseInt(event.currentTarget.dataset.imageId);
+      $('.single-item').data("imageId", id);
+      $('.single-item').slick('slickGoTo', id);
+    })
+  })
+  $(document).on('click', (event) => {
     $('.single-item').slick();
+  });
+
+  $('#indexModal').on('shown.bs.modal', () => {
+    const id = $('.single-item').data("imageId");
+    $('.single-item').slick('slickGoTo', id, true);
+    $('.single-item').slick('setPosition');
+    $('.single-item').css({ opacity: 1 });
   });
 }
 
